0ae7d7bc3eca3a5546b1ecff3a89900174111a07
Vinu Andrews S <Andrewsai01@brightskills.com>
2024-01-20 10:27:37 +0530

    classandobjectdemo

---
+++ main.cpp
@@ -0,0 +1,68 @@
+
+#include <iostream>
+using namespace std;
+
+class secondsource
+{
+public :
+        float a,b,c;
+        int multiplication();
+        int division();
+
+
+        int addition()
+{
+        cout<<"\n---Addition Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a+b;
+
+        cout<<"\nThe Added Value of " <<a <<" + " <<b<< " is:"<<c<<"\n";
+}
+        int subtraction()
+{
+        cout<<"\n---Subtraction Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a-b;
+        cout<<"\nThe Subtracted Value of " <<a <<" - " <<b<< " is:"<<c<<"\n";
+}
+};
+int secondsource :: multiplication()
+{
+    float a, b,c;
+        cout<<"\n---Multiplication Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a*b;
+        cout<<"\nThe Multiplied Value of " <<a <<" * " <<b<< " is:"<<c<<"\n";
+}
+
+int secondsource :: division()
+{
+    float a, b, c;
+        cout<<"\n---Division Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a/b;
+        cout<<"\nThe divided Value of " <<a <<" / " <<b<< " is:"<<c<<"\n";
+}
+int main()
+{
+ secondsource rk;
+rk.addition();
+rk.subtraction();
+rk.multiplication();
+rk.division();
+
+    return 0;
+}
+
